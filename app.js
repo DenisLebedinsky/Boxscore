98b218cb27a81f7dbc7f3dcf8ca3a9bcc5c59c46
Den Lebedinsky <Den.Lebedinsky@gmail.com>
2019-05-06 17:43:22 +0300

    start

---
+++ app.js
@@ -0,0 +1,39 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var schedule = require("node-schedule");
+
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+// shedule 
+var j = schedule.scheduleJob("*/15 * * * * *", function() {
+  console.log("The answer to life, the universe, and everything!");
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
