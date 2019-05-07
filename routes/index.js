ad0cd1e88edd54258c8cfcacaa6717238d67cb20
Den Lebedinsky <Den.Lebedinsky@gmail.com>
2019-05-07 15:11:37 +0300

    add server endpoin

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,57 @@
-const express = require("express");
-const router = express.Router();
-const StatsNBA = require("../models/nba").StatsNBA;
-const StatsMLB = require("../models/mlb").StatsMLB;
-const getData = require("../controllers/getData");
+const express = require('express')
+const router = express.Router()
+const StatsNBA = require('../models/nba').StatsNBA
+const StatsMLB = require('../models/mlb').StatsMLB
+const getData = require('../controllers/getData')
+const converter = require('../controllers/converter')
+const controller = require('../controllers/dbController')
 
-router.get('/nba', function(req, res, next) {
-	StatsNBA.find({}, async function(err, dataArr){
-		if(err) throw err;
-		const data = dataArr[0];
-		if ((new Date() -  data.updated_at)/1000 > 15){
-			const nba = await getData.NBA();
-			controller.saveUpdateNBA(nba.data);
-			//res.send
-		} else {
-			//send data
-		}
-	})
-});
-
-router.get("/nba", function(req, res, next) {
-	StatsMLB.find({}, async function(err, dataArr) {
-    if (err) throw err;
-    const data = dataArr[0];
-    if ((new Date() - data.updated_at) / 1000 > 15) {
-      const mlb = await getData.MLB();
-      controller.saveUpdateMLB(mlb.data);
-      //res.send
+router.get('/:league', function(req, res, next) {
+    if (req.params.league === 'nba') {
+        StatsNBA.find({}, async function(err, dataArr) {
+            if (err) throw err
+            const data = dataArr[0]
+            if (
+                Math.abs(new Date().getTime() - data.updated_at.getTime()) /
+                    1000 >
+                15
+            ) {
+                const nba = await getData.NBA()
+                await controller.saveUpdateNBA(nba.data)
+                const dataToClient = converter.toClientNBA(nba.data)
+                res.send(dataToClient)
+            } else {
+                StatsNBA.find({}, function(err, response) {
+                    if (err) throw err
+                    const dataToClient = converter.toClientNBA(response[0].data)
+                    res.send(dataToClient)
+                })
+            }
+        })
+    } else if (req.params.league === 'mlb') {
+        StatsMLB.find({}, async function(err, dataArr) {
+            if (err) throw err
+            const data = dataArr[0]
+            if (
+                Math.abs(new Date().getTime() - data.updated_at.getTime()) /
+                    1000 >
+                15
+            ) {
+                const mlb = await getData.MLB()
+                await controller.saveUpdateMLB(mlb.data)
+                const dataToClient = converter.toClientMLB(mlb.data)
+                res.send(dataToClient)
+            } else {
+                StatsMLB.find({}, function(err, response) {
+                    if (err) throw err
+                    const dataToClient = converter.toClientMLB(response[0].data)
+                    res.send(dataToClient)
+                })
+            }
+        })
     } else {
-      //send data
+        res.send('invalid params').status(403)
     }
-  });
-});
+})
 
-module.exports = router;
+module.exports = router
--- seduler.js
+++ seduler.js
@@ -1,14 +1,14 @@
-const schedule = require("node-schedule");
-const getData = require("./controllers/getData");
-var controller = require("./controllers/dbController");
+const schedule = require('node-schedule')
+const getData = require('./controllers/getData')
+var controller = require('./controllers/dbController')
 
 // shedule
-schedule.scheduleJob("*/15 * * * * *", async function() {
-	const nba = await getData.NBA();
-	controller.saveUpdateNBA(nba.data);
+schedule.scheduleJob('*/15 * * * * *', async function() {
+    const nba = await getData.NBA()
+    controller.saveUpdateNBA(nba.data)
 
-	const mlb = await getData.MLB();
-	controller.saveUpdateMLB(mlb.data);
-});
+    const mlb = await getData.MLB()
+    controller.saveUpdateMLB(mlb.data)
+})
 
-module.exports = schedule;
+module.exports = schedule
