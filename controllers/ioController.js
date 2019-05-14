0af65c03dd532c358fdfda9fe057d05244899b4d
Den Lebedinsky <Den.Lebedinsky@gmail.com>
2019-05-14 16:46:12 +0300

    change socket connect path

--- controllers/ioController.js
+++ controllers/ioController.js
@@ -1,11 +1,11 @@
 const eventEmmiter = require('../eventEmmitter')
 module.exports = {
     createIo: server => {
-        const io = require('socket.io')(server)
+				const io = require('socket.io')(server)
+				io.path('/socket');
         io.on('connection', function(socket) {
             eventEmmiter.on('notify', data => {
                 data.forEach(el => {
-                    console.log(el.league)
                     socket.emit('update_data_' + el.league, el)
                 })
             })
